0a6ea207cd3e73066a416f774778dfac0b4ddb50
Andyj10 <57446508+Andyj10@users.noreply.github.com>
2019-11-11 17:45:47 -0300

    v1

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/main/header/header.component';
+import { ContentComponent } from './component/main/content/content.component';
+import { FooterComponent } from './component/main/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
